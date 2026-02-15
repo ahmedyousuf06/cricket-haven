c6e5256f8e7f6f7fef03478a84af232ae0b3c55f
Muhammad Ahmed <muhammad.csu@gmail.com>
2026-02-15 14:39:56 -0600

    fix(frontend): add Vite React refresh preamble

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config('app.name', 'Cricket Haven') }}</title>
+        @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     </head>
     <body class="bg-slate-50 antialiased">
